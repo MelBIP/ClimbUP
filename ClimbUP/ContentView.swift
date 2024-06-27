de318b8dfde24f5d670320f0f7cc861b639e62f9
MelBIP <s22005@gsm.hs.kr>
2024-06-27 18:08:56 +0900

    🎨 :: ColorSets 설정

--- ClimbUP/ContentView.swift
+++ ClimbUP/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             if showMainView {
-//                MainView()
+                MainView()
             } else {
                 SplashView()
                     .onAppear {
