9ec319f8a73fdb4262a4fe247638b525e9b5e5be
MelBIP <s22005@gsm.hs.kr>
2024-08-20 20:54:50 +0900

    ✏️ :: 페이지 전환

--- ClimbUP/Main/MainView.swift
+++ ClimbUP/Main/MainView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var isSignInViewPresented = false
+    @State var isSignUpViewPresented = false
+    
     var body: some View {
         VStack {
             Spacer()
@@ -12,29 +15,42 @@ struct MainView: View {
             
             VStack {
                 Button(action: {
-                    print("Test")
+                    print("로그인 페이지로 이동")
+                    
+                    self.isSignInViewPresented.toggle()
                 }, label: {
                     Text("로그인")
                         .foregroundStyle(.white)
                         .font(.psb17)
-                        .frame(width: 353, height: 53)
-                        .background(Color.mainBlue)
-                        .cornerRadius(8)
                 })
+                .fullScreenCover(isPresented: $isSignInViewPresented) {
+                    SignInView()
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.mainBlue)
+                .cornerRadius(8)
+                .padding(.horizontal, 20)
                 .padding(.bottom, 8)
                 
                 Button(action: {
-                    print("회원가입")
+                    print("회원가입 페이지로 이동")
+                    
+                    self.isSignUpViewPresented.toggle()
                 }, label: {
                     Text("회원가입")
                         .foregroundStyle(.gray)
                         .font(.psb14)
                 })
                 .padding(.bottom, 32)
+                .fullScreenCover(isPresented: $isSignUpViewPresented) {
+                    SignUpView()
+                }
             }
         }
     }
 }
+
 
 #Preview {
     MainView()
