d4e2d4fca1ce0cd74898138cd69c1c484fb5f11a
MelBIP <s22005@gsm.hs.kr>
2024-09-05 11:30:47 +0900

    🎨 :: Modify FontName

--- ClimbUP/Extension/Extension + Font.swift
+++ ClimbUP/Extension/Extension + Font.swift
@@ -2,24 +2,24 @@ import SwiftUI
 
 extension Font {
     // Bold
-    static let ab14: Font = .custom("AppleSDGothicNeoB", size: 14)
-    static let ab16: Font = .custom("AppleSDGothicNeoB", size: 16)
-    static let ab18: Font = .custom("AppleSDGothicNeoB", size: 18)
-    static let ab20: Font = .custom("AppleSDGothicNeoB", size: 20)
-    static let ab24: Font = .custom("AppleSDGothicNeoB", size: 24)
-    static let ab40: Font = .custom("AppleSDGothicNeoB", size: 40)
+    static let ab14: Font = .custom("AppleSDGothicNeo-Bold", size: 14)
+    static let ab16: Font = .custom("AppleSDGothicNeo-Bold", size: 16)
+    static let ab18: Font = .custom("AppleSDGothicNeo-Bold", size: 18)
+    static let ab20: Font = .custom("AppleSDGothicNeo-Bold", size: 20)
+    static let ab24: Font = .custom("AppleSDGothicNeo-Bold", size: 24)
+    static let ab40: Font = .custom("AppleSDGothicNeo-Bold", size: 40)
 
     // SemiBold
-    static let asb11: Font = .custom("AppleSDGothicNeoSB", size: 11)
-    static let asb12: Font = .custom("AppleSDGothicNeoSB", size: 12)
-    static let asb14: Font = .custom("AppleSDGothicNeoSB", size: 14)
-    static let asb17: Font = .custom("AppleSDGothicNeoSB", size: 17)
+    static let asb11: Font = .custom("AppleSDGothicNeo-SemiBold", size: 11)
+    static let asb12: Font = .custom("AppleSDGothicNeo-SemiBold", size: 12)
+    static let asb14: Font = .custom("AppleSDGothicNeo-SemiBold", size: 14)
+    static let asb17: Font = .custom("AppleSDGothicNeo-SemiBold", size: 17)
     
     // Medium
-    static let am16: Font = .custom("AppleSDGothicNeoM", size: 16)
+    static let am16: Font = .custom("AppleSDGothicNeo-Medium", size: 16)
 
     // Regular
-    static let ar12: Font = .custom("AppleSDGothicNeoR", size: 12)
-    static let ar14: Font = .custom("AppleSDGothicNeoR", size: 14)
-    static let ar16: Font = .custom("AppleSDGothicNeoR", size: 16)
+    static let ar12: Font = .custom("AppleSDGothicNeo-Regular", size: 12)
+    static let ar14: Font = .custom("AppleSDGothicNeo-Regular", size: 14)
+    static let ar16: Font = .custom("AppleSDGothicNeo-Regular", size: 16)
 }
